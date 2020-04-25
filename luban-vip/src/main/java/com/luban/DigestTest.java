724566a6ca6ebb5403b4c4b83b26b746eb8a89ac
boomblog <497269678@qq.com>
2020-04-25 15:52:50 +0800

    lubanvip

---
+++ luban-vip/src/main/java/com/luban/DigestTest.java
@@ -0,0 +1,12 @@
+package com.luban;
+
+import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
+
+import java.security.NoSuchAlgorithmException;
+
+public class DigestTest {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        System.out.println(DigestAuthenticationProvider.generateDigest("super:admin"));
+    }
+}
